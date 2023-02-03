67902343ee931b1c5356820c01df4bce169ef449
Rod <rodolforoveroto@gmail.com>
2023-02-03 18:53:03 -0300

    removendo codigos não utilizados

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,8 @@ import './App.css'
 import { Callendar } from './pages/Callendar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Callendar />
   )
 }
-
 export default App
